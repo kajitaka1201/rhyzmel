ca90cb1c47190a6143c0178162e1ffa5195ecbbd
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-04-30 09:34:55 +0000

    feat:open form os file app

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,13 +1,22 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { uploadFile } from "@/lib/upload";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PlayList from "@/components/ui-elements/playlist";
 import Player from "@/components/ui-elements/player";
 
 export default function App() {
   const [files, setFiles] = useState<File[]>([]);
   const [playingIndex, setPlayingIndex] = useState<number>(0);
+  useEffect(() => {
+    const windowWithLaunchQueue = window as WindowWithLaunchQueue;
+    windowWithLaunchQueue?.launchQueue?.setConsumer(
+      async ({ files }: { files: FileSystemFileHandle[] }) => {
+        console.log(files);
+        setFiles(await Promise.all(files.map(f => f.getFile())));
+      }
+    );
+  }, []);
   function upload() {
     uploadFile().then(res => {
       console.log(res);
